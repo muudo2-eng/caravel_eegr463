b48df4a618cc13c78938c9753e26cbc94033b41e
Marcus Bazemore <mabaz5@morgan.edu>
2026-04-01 23:31:56 -0400

    Add files via upload

--- temp_reader.sv
+++ temp_reader.sv
@@ -4,16 +4,27 @@ input reset,
 
 input  [7:0] temp,
 output reg [7:0] temperature,
-output reg fever_flag);
+output reg fever_flag,
+output reg data_valid,
+output reg tx_request);
 always @(posedge clk)
 begin 
+    data_valid <= 0;
+    tx_request <= 0;
     if (reset) begin
         fever_flag <= 0;
-        temperature <= 0;
+        temperature <= 0; 
+        
+
     end
     else  begin
-        temperature <= temp;
-        if (temp < 8'd25)
+        if (temp != temperature) begin
+            data_valid <= 1;
+            tx_request <= 1;
+            temperature <= temp;
+        end
+
+        if (temp > 8'd98)
             fever_flag <= 1;
         else
             fever_flag <= 0;
--- temp_reader_tb.sv
+++ temp_reader_tb.sv
@@ -6,20 +6,29 @@ reg reset;
 reg [7:0] temp;
 wire [7:0] temperature; 
 wire fever_flag;
+wire data_valid;
+wire tx_request;
  
-temp_reader tr1 (.clk(clk), .reset(reset),.temp(temp),.temperature(temperature),.fever_flag(fever_flag));
+temp_reader tr1 (.clk(clk), .reset(reset),.temp(temp),.temperature(temperature),.fever_flag(fever_flag),.data_valid(data_valid),.tx_request(tx_request));
 
 always #25 clk = ~clk;
 initial begin
     clk = 0;
     reset = 1;
-    #20 reset =0; 
-    forever begin
+    #40 reset =0; 
+  forever begin
     temp = 8'd01;
-    #20 temp = 8'd25;
-    #20 temp = 8'd35;
-    #20 temp = 8'd50;    
-    end
+    #35  temp = 8'd95;
+    #55  temp = 8'd96;
+    #45  temp = 8'd97;
+    #65  temp = 8'd98;
+    #40  temp = 8'd99;
+    #70  temp = 8'd100;
+    #50  temp = 8'd101;
+    #60  temp = 8'd102;
+    #45  temp = 8'd103;
+    #55;
+end
     
 end
 endmodule
